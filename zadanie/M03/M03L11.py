fe3fd7e7f2a05a4f881bde5cc8712a42dfbc8e44
PiotrSaltoPanda <lacrimosearena@gmail.com>
2025-08-03 09:34:23 +0200

    M03L11: Dodano rozwiązanie prowadzącego.

--- zadanie/M03/M03L11.py
+++ zadanie/M03/M03L11.py
@@ -1,19 +1,19 @@
-import glob
+# import glob
 
-files = glob.glob('M03/M03L0*')
-NEW_EXTENSION = '.bak'
+# files = glob.glob('M03/M03L0*')
+# NEW_EXTENSION = '.bak'
 
-for file in files:
-    if '.' in file:
-        tokens = file.rsplit('.', maxsplit=1)
-        name = tokens[0]
-        extension = tokens[1]
-    else:
-        name = file
-        extension = ''
+# for file in files:
+#     if '.' in file:
+#         tokens = file.rsplit('.', maxsplit=1)
+#         name = tokens[0]
+#         extension = tokens[1]
+#     else:
+#         name = file
+#         extension = ''
 
-    new_filename = name + NEW_EXTENSION
-    print(file, '->', new_filename)
+#     new_filename = name + NEW_EXTENSION
+#     print(file, '->', new_filename)
 
 #-------------------------------------------------------------
 
@@ -29,3 +29,26 @@ for file in files:
 #         print(file, '->', new_filename)
 #     else:
 #         print(file, '->', file + NEW_EXTENSION)
+
+#------------------------------------------------------------
+
+# rozwiązanie:
+
+# import glob
+
+# NEW_EXTENSION = '.bak'
+
+# pattern = input("Podaj pattern: ")
+# filenames = glob.glob(pattern)
+
+# for filename in filenames:
+#     if '.' in filename:
+#         tokens = filename.split('.', maxsplit=1)
+#         name = tokens[0]
+#         extension = tokens[1]
+#     else:
+#         name = filename
+#         extension = ''
+
+#     new_filename = name + NEW_EXTENSION
+#     print(filename, '->', new_filename)
